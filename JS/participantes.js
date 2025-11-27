d9aaced555fc0db95539c8044ced39e3ddff0dcf
Gabriel <gabdel.azbe7@gmail.com>
2025-11-27 13:09:21 +0100

    cambio14

--- JS/participantes.js
+++ JS/participantes.js
@@ -1,9 +1,9 @@
-
 const navbarBoton = document.querySelector(".btn-nav");
 const navbarList = document.querySelector(".nav-list");
 
-        navbarBoton.addEventListener("click", () => {
-    navbarBoton.classList.toggle("active");
+
+navbarBoton.addEventListener("click", () => {
+  navbarBoton.classList.toggle("active");
   if (navbarList.classList.contains("active")) {
     navbarList.classList.remove("active");
     navbarList.classList.add("close");
@@ -14,4 +14,11 @@ const navbarList = document.querySelector(".nav-list");
   } else {
     navbarList.classList.add("active");
   }
+});
+document.querySelectorAll(".likes").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const icon = btn.querySelector("span");
+        icon.classList.toggle("bi-heart");
+        icon.classList.toggle("bi-heart-fill");
+    });
 });
